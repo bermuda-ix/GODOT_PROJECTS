1ed3c6c1a729da2b002aa80ac53fca4eb71f9f04
bermuda-ix <bermuda_ix@protonmail.com>
2025-12-25 13:47:31 -0600

    Refactored load and save functions into instantiated components

--- FirstProj/MainProjFolder/levels/world.gd
+++ FirstProj/MainProjFolder/levels/world.gd
@@ -12,6 +12,8 @@ extends Node2D
 @onready var pause_menu = $CanvasLayer/PauseMenu
 @onready var score : int = 0
 @onready var heat_handler: HeatHandler = $HeatHandler
+@onready var hit_stop: HitStop = $HitStop
+@onready var level_state_handler: LevelStateHandler = $LevelStateHandler
 
 
 #Cutscenes
@@ -57,10 +59,8 @@ func _ready():
 	Events.pause.connect(show_pause)
 	Events.unpause.connect(unpause)
 	Events.inc_score.connect(inc_score)
-	Events.checkpoint_reached.connect(save_level_state)
 	Events.load_checkpoint.connect(reload_scene)
 	#GlobalSaveData.save_game()
-	
 	spawn_points = get_tree().get_nodes_in_group("SpawnPoint")
 	heat_handler.heat_lvl_spawn()
 	for i in spawn_points.size():
@@ -69,6 +69,7 @@ func _ready():
 	#Events.end_cutsene.connect(end_cutscene)
 	#Events.queue_cutscene.emit(Cutscenes.intro_cutscene)
 	#score=45
+	
 	init_active_enemies_list = get_tree().get_nodes_in_group("Enemy")
 	if lvl_type=="adv":
 		cutscene_player.play("TEST")
@@ -76,7 +77,9 @@ func _ready():
 		Events.spawn_update.emit(enemy_list.REG_ENEMIES, true)
 		end_cutscene()
 	
-	load_level_state()
+	level_state_handler.load_level_state()
+	PC.global_position=default.global_position
+	hit_stop.end_hit_stop()
 	
 func _process(_delta):
 	
@@ -109,66 +112,7 @@ func _physics_process(delta: float) -> void:
 	if not cutscene_active and not camera_pos.stationary:
 		camera_pos.global_position=Vector2(player.global_position.x, player.global_position.y-50)
 	
-func save_level_state():
-	var _save_file = FileAccess.open("user://savegame.save", FileAccess.WRITE)
-	var persistant_nodes : Array[Node] =get_tree().get_nodes_in_group("Persistant")
-	for node in persistant_nodes:
-		if node.scene_file_path.is_empty():
-			print("persistent node '%s' is not an instanced scene, skipped" % node.name)
-			continue
-		#if !node.has_method("save_state"):
-			#print("persistent node '%s' is missing a save() function, skipped" % node.name)
-			#continue
-		#node.call("save_state")
-		Events.save_states.emit()
-	var _active_enemies_list : Array[Node] = get_tree().get_nodes_in_group("Enemy")
-	var _active_enemy_names : Array[StringName]
-	for _node in _active_enemies_list:
-		_active_enemy_names.push_back(_node.name)
-	var _active_enemies_json=JSON.stringify(_active_enemy_names)
-	print(_active_enemies_json)
-	_save_file.store_line(_active_enemies_json)
 
-
-
-func load_level_state():
-#	Update enemies to remove enemies from tree that are no longer active, based on what was saved
-	var _save_file = FileAccess.open("user://savegame.save", FileAccess.READ)
-	var _active_enemy_list : Array
-	while _save_file.get_position() < _save_file.get_length():
-		var _json_string = _save_file.get_line()
-		var _json = JSON.new()
-		var _parse_result = _json.parse(_json_string)
-		if not _parse_result == OK:
-			print("JSON Parse Error: ", _json.get_error_message(), " in ", _json_string, " as line ", _json.get_error_line())
-			continue
-		var _parse_data=_json.data
-		if typeof(_parse_data)==TYPE_ARRAY:
-			_active_enemy_list=_parse_data
-	for _enemy in init_active_enemies_list:
-		if is_instance_valid(_enemy):
-			if _active_enemy_list.has(_enemy.name):
-				continue
-			else:
-				_enemy.queue_free()
-				
-#	Update persistant objects sates based on what was saved
-	GlobalSaveData.load_game()
-	var _persistant_nodes : Array[Node] =get_tree().get_nodes_in_group("Persistant")
-	for _node in _persistant_nodes:
-		var _name=_node.get_path()
-		if _node.scene_file_path.is_empty():
-			print("persistent node '%s' is not an instanced scene, skipped" % _node.name)
-			continue
-		if not GlobalSaveData.level_state["persistence"].has(str(_name)):
-			print("persistent node '%s' is not in persistence dictionary, skipped" % _node.name)
-			continue
-		
-		#print(GlobalSaveData.level_state["persistence"][str(_name)])
-		var _state=GlobalSaveData.level_state["persistence"][str(_name)]
-		Events.load_states.emit(_state)
-		#_node.load_state(_state)
-	#Events.load_checkpoint.emit()
 		
 func reload_scene():
 	get_tree().reload_current_scene()
