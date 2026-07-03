b7cf4be5cc161e557dbb4f1bc4b2f919466966c1
bermuda-ix <bermuda_ix@protonmail.com>
2026-07-03 16:45:12 -0500

    Update heavy_soldier.gd
    Hitting shield temporarily de-actives hurt box

--- FirstProj/MainProjFolder/chars/heavy_soldier.gd
+++ FirstProj/MainProjFolder/chars/heavy_soldier.gd
@@ -704,7 +704,7 @@ func _on_hit_box_clashed() -> void:
 
 
 func _on_shield_area_entered(area: Area2D) -> void:
-	health.set_temporary_immortality(0.5)
+	hurt_box.set_active(false)
 
 
 func _on_hit_box_clash_knock_back(_launch : float, _knockback : float, _impact_dir_right : bool) -> void:
