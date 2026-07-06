fea405d816f48fc410aad0e5b54755b9b44b3c48
bermuda-ix <bermuda_ix@protonmail.com>
2026-07-06 17:47:00 -0500

    Bug fixing being stuck in wall stick state

--- FirstProj/MainProjFolder/pc_wall_stick.gd
+++ FirstProj/MainProjFolder/pc_wall_stick.gd
@@ -11,6 +11,11 @@ func _update(delta: float) -> void:
 	if pc.state_machine.get_active_state()==pc.wall_stick:
 		if pc.is_on_floor():
 			pc.state_machine.dispatch(&"return_to_idle")
+	
+	if Input.is_action_just_released("sprint"):
+		pc.state_machine.dispatch(&"fall_off_wall")
+	if Input.is_action_just_pressed("jump"):
+		pc.state_machine.dispatch(&"jump_off_wall")
 
 func _exit() -> void:
 	pc.wall_hold = false
